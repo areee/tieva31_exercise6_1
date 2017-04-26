3042d3200b56775fa5ba2aca47f8710fae31c310
areee <areee@users.noreply.github.com>
2017-04-26 23:09:15 +0300

    exercise done

---
+++ src/BirthYear.java
@@ -0,0 +1,34 @@
+
+import javax.swing.AbstractSpinnerModel;
+
+/**
+ *
+ * @author ylhaart
+ */
+public class BirthYear extends AbstractSpinnerModel {
+
+    public BirthYear() {
+        super();
+    }
+
+    @Override
+    public Object getValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setValue(Object value) {
+        // not set
+    }
+
+    @Override
+    public Object getNextValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getPreviousValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
